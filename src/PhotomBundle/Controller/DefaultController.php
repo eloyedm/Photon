cc1957f0fe15687fac9fc8a2d0a75788d2f55ece
eloyedm <eloy.edm@gmail.com>
2016-09-28 22:31:36 -0500

    conexion a la base de datos establecida

--- src/PhotomBundle/Controller/DefaultController.php
+++ src/PhotomBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace PhotomBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use \PDO;
 
 class DefaultController extends Controller
 {
@@ -13,6 +14,10 @@ class DefaultController extends Controller
     public function indexAction()
     {
         $photon = "hola a todos";
+        $conn =$this->get('database_connection');;
+        $users = $conn->fetchAll("SELECT * FROM Usuario");
+        dump($users);
+        die();
         return $this->render('PhotomBundle:Default:index.html.twig', array("photon" => $photon));
     }
 }
