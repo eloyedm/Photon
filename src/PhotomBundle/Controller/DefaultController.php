e66485c19f091d91a360cac7810de5db7098ce05
eloyedm <eloy.edm@gmail.com>
2016-11-06 00:01:36 -0600

    pagina de estilo lista

--- src/PhotomBundle/Controller/DefaultController.php
+++ src/PhotomBundle/Controller/DefaultController.php
@@ -18,7 +18,7 @@ class DefaultController extends Controller
     public function connectToDB(){
         $servername = "localhost";
         $username = "root";
-        $password = "";
+        $password = "homecoming96";
         $conn = new PDO("mysql:host=$servername;dbname=photon", $username, $password);
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
